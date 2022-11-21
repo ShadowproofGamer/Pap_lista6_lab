4c583e150bd845efcf3078d9c10774a75cdd4897
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2022-11-21 18:05:12 +0100

    zad1

--- src/zad1.sc
+++ src/zad1.sc
@@ -1,11 +1,27 @@
-sealed trait Plec
-case object Kobieta extends Plec
-case object Mezczyzna extends Plec
 
-
-
-{
-  sealed trait Status_osoby
+  sealed trait Plec
   case object Kobieta extends Plec
   case object Mezczyzna extends Plec
-  |}
+
+
+  sealed trait Status_osoby
+  case object Dziecko extends Status_osoby
+  case object Uczen extends Status_osoby
+  case object Student extends Status_osoby
+  case object Pracownik extends Status_osoby
+  case object Emeryt extends Status_osoby
+
+
+def definicja(tuple:(String, String, Plec, Int, String)):Status_osoby = {
+  tuple._4 match
+    case _ if tuple._5 != "" || (tuple._4>=26 && ((tuple._4 <= 60 && tuple._3 == Kobieta)||(tuple._4 <= 65 && tuple._3 == Mezczyzna))) => Pracownik
+    case tuple._4 if tuple._4<7 => Dziecko
+    case tuple._4 if tuple._4>=7 && tuple._4<=19 => Uczen
+    case tuple._4 if tuple._4>=20 && tuple._4<=25 => Student
+    case _ => Emeryt
+}
+
+  definicja(("Anna", "Cebula", Kobieta, 55, "LO6"));
+  definicja(("Adam", "Buczylko", Mezczyzna, 84, ""))
+  definicja(("Jakub", "Cebula", Mezczyzna, 20, ""))
+  definicja(("Janina", "Buczylko", Kobieta, 84, "PWr"))
--- src/zad2.ml
+++ src/zad2.ml
@@ -1,14 +1,20 @@
-type samochod = string * (string * int);;
+type samochod = (string * (string * int));;
 let listaSamochodow = [("Opel", ("astra",1999)); ("Renault", ("megane", 2004)); ("Opel", ("corsa", 2009)); ("Nissan",( "micra", 2004)); ("Opel", ("corsa", 2009)); ("Nissan", ("micra", 2004))];;
 
 let znajdzSamochod list wartosc =
-  let zsumuj lista wynik =
-    let element = (List.hd lista)
-    in match element with
-         element when (List.filter(fun x -> element=x)=[]) -> ele
-       |_ -> (fst(element), (fst(snd(element)), 1))::wynik
-      
-(List.filter (fun x -> snd(snd(x)) = wartosc) list);;
+  let rec count xs res ={
+      if xs=[]
+         else fold_Left 0 
+    let rec findInList elem xss acc =
+      match xss with
+        h::t when h=elem -> findInList elem (List.tl xss) acc+1
+      |[]->acc
+      |_ -> findInList elem (List.tl xss) acc
+        in (fst(List.hd xs),fst(snd(List.hd xs)),findInList (List.hd xs) (List.tl xs) 1)::res
+                                                                                            count (List.tl xs) res
+    }
+  in count (List.filter (fun x -> snd(snd(x)) = wartosc) list) [];;
+
 
 znajdzSamochod listaSamochodow 2004;;
 
